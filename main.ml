6830bbb02d22685b4e95b168c98f1399df95065d
Taisuke Nakano <taisuke@Taisukes-MacBook-Air.local>
2024-11-11 21:27:31 -0500

    Trying to implement the generate_candidate function.

--- main.ml
+++ main.ml
@@ -1,20 +1,32 @@
-exception not_found
+(* Exception in case we couldn't find a solution. *)
+exception NotFound
 
+(* Type definition *)
 type grid = int list list (* Grid is represented as a matrix. *)
 type 'a tree = Empty | Node of ('a tree) list * 'a
-type hints = (int list list) * (int list list) (* horizontal and vertical *)
+type 'a pair = 'a * 'a
+type hints = (int list list) pair (* horizontal and vertical *)
 
-let first tuple = 
-    match tuple with
+(* Returns the first value of the pair *)
+let first p: 'a pair = 
+    match p with
     | (x, y) -> x
 
-let second tuple = 
-    match tuple with
+(* Returns the first value of the pair *)
+let second p: 'a pair = 
+    match p with
     | (x, y) -> y
 
 let list_sum l = 
     List.fold_left (+) 0 l
 
+(*  *)
+let rec generate_candidates (hint: int list) grid_length = 
+    let rec insert_spaces remaining_blocks remaining_spaces acc = 
+        match remaining_blocks with
+        | [] -> 
+
+(*
 (* depth is 0-indexed. *)
 let rec search (input: hints) (current: grid tree) (depth: int) = 
     match current with
@@ -24,7 +36,7 @@ let rec search (input: hints) (current: grid tree) (depth: int) =
             let horizontal_hint = first hints
             let hint_size = List.length (List.nth (second hints) depth) in
             let hint_sum = list_sum 
-
+*)
 let main () = 
     let test_inputs: hints list = 
         [([[3];[1];[3];[2];[4]], [[1];[1;1];[3;1];[3];[3]])] in
